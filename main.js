9262e7e85fc22084b9465a5c5847ff48b93d8842
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2021-10-21 15:51:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var menu=["chicken Tandoori Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza", "Chicken Supreme Pizza"]
+
+function show(){
+    document.getElementById("label").innerHTML=menu;
+}
